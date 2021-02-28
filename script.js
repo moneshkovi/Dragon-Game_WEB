517cbf71a4f6087b101c2b132ea28adf68d39fef
Monesh kovi <67748834+moneshkovi@users.noreply.github.com>
2021-02-28 19:30:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ cross = true;
 
 audio = new Audio('music.mp3');
 audiogo = new Audio('gameover.mp3');
-setTimeout(() => {
+setTimeout(() => { 
     audio.play()
 }, 1000);
 document.onkeydown = function (e) {
